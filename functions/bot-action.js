f56317170a04939185fbecc22d170f74a73b3ad1
BunnY <bunny@findsolutiongroup.com>
2022-05-21 15:51:58 +0800

    add double confirm for unsubscribe

--- functions/bot-action.js
+++ functions/bot-action.js
@@ -1,8 +1,36 @@
-const {bot, stripe} = require('./utils');
+const {Markup} = require('telegraf');
+const {bot, stripe, randomArray} = require('./utils');
 
 require('./bot-action');
-bot.action(/unsubscribe_+/, async (ctx) => {
-  const subscribeId = ctx.match.input.substr('unsubscribe_'.length);
+// 1. unsubscribe
+bot.action(/confirm_unsubscribe_+/, async (ctx) => {
+  const subscribeId = ctx.match.input.substr('confirm_unsubscribe_'.length);
+  const escUnsubscribe = (text) =>
+    Markup.button.callback(text, 'unsubscribe_esc');
+  const buttons = [
+    [escUnsubscribe('Nope, nevermind')],
+    [escUnsubscribe('No No No!')],
+    [escUnsubscribe('No')],
+    [
+      Markup.button.callback(
+        "Yes I'm 100% sure",
+        `unsubscribe_process_${subscribeId}`,
+      ),
+    ],
+  ];
+  await ctx.editMessageText(
+    '⚠️ Are you sure to cancel your subscription?',
+    Markup.inlineKeyboard(randomArray(buttons)),
+  );
+});
+// 2.1 unsubscribe: esc
+bot.action('unsubscribe_esc', async (ctx) => {
+  const text = '🔄 Your subscription was not cancelled, it still alive\n';
+  return await ctx.editMessageText(text);
+});
+// 2.2 unsubscribe: process
+bot.action(/unsubscribe_process_+/, async (ctx) => {
+  const subscribeId = ctx.match.input.substr('process_unsubscribe_'.length);
   const unsubscribe = await stripe.subscriptions.del(subscribeId);
   if (unsubscribe.status === 'canceled') {
     return await ctx.editMessageText('✅ Canceled subscription');

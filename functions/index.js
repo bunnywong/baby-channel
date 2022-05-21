bbd5947f1b8ed2808463eb5b63974a3201cbb070
BunnY <bunny@findsolutiongroup.com>
2022-05-21 16:03:05 +0800

    clean up index

--- functions/index.js
+++ functions/index.js
@@ -2,14 +2,6 @@ const functions = require('firebase-functions');
 const {bot} = require('./utils');
 require('./bot');
 const endpoints = require('./endpoints');
-// const _priceCode = 'price_1KwVE0GVMqzrNuvfiIhntx8J';
-// # ref URL:
-// https://core.telegram.org/bots/api
-// # Telegraf
-// example - https://github.com/telegraf/telegraf/tree/bafef538c228db581352746539c7ccfacb099b64/docs/examples
-// # membership 101
-// https://medium.com/invitemember/membership-business-in-telegram-the-definitive-guide-68a1afcd456b
-// https://godleon.github.io/blog/ChatOps/howto-develop-telegram-chatbot/
 
 module.exports = {
   bot: functions.https.onRequest(async (request, response) => {
